edc46418aa4c91838b356373c2679d6ff8c807e9
bastiaanv <bastiankpn7800@gmail.com>
2021-02-24 21:38:43 +0100

    Added large file upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const MAX_SIZE = 10485759;
 const fetch = require('node-fetch');
 fetch(`https://login.microsoftonline.com/${process.env.TENANTID}/oauth2/v2.0/token`, {
     method: 'POST',
@@ -12,7 +13,7 @@ fetch(`https://login.microsoftonline.com/${process.env.TENANTID}/oauth2/v2.0/tok
 }).then((res) => {
     const fs = require('fs');
 
-    fs.readdir(process.cwd(), function (err, files) {
+    fs.readdir(process.cwd(), async function (err, files) {
         if (err) {
           console.log(err);
           return;
@@ -21,24 +22,37 @@ fetch(`https://login.microsoftonline.com/${process.env.TENANTID}/oauth2/v2.0/tok
         const filePath = files.find(x => x.includes('.zip'));
         if (filePath) {
             const stats = fs.statSync(filePath).size;
-            const stream = fs.createReadStream(filePath);
 
-            fetch(`https://graph.microsoft.com/v1.0/me/drive/items/${process.env.FOLDERID}:/${filePath}:/content`, {
-                method: 'PUT',
+            // First create upload session
+            const session = await fetch(`https://graph.microsoft.com/v1.0/me/drive/items/${process.env.FOLDERID}:/${filePath}:/createUploadSession`, {
+                method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${res.access_token}`,
-                    "Content-length": stats
                 },
-                body: stream
-            })
-            .then(function(res) {
-                console.log(res.status)
-                return res.text();
-            }).then(function(json) {
-                console.log(json);
-            }).catch((err) => {
-                console.log(err)
+            }).then(function(res) {
+                console.log(res.status);
+                return res.json();
             });
+            
+            // upload file
+            for (let i = 0; i < stats; i+=(MAX_SIZE+1)) {
+                console.log(`bytes: ${i} till ${i+MAX_SIZE}`)
+                const end = Math.min(i+MAX_SIZE, stats-1);
+                const stream = fs.createReadStream(filePath, {start: i, end });
+
+                await fetch(session.uploadUrl, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${res.access_token}`,
+                        "Content-length": end-i+1,
+                        "Content-Range": `bytes ${i}-${end}/${stats}`
+                    },
+                    body: stream
+                });
+            }
+
+            // Complete upload
+            await fetch(session.uploadUrl, {method: 'POST'}).then(function(res) { return res.json() });
         }
     });
 });
